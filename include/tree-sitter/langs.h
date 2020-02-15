78c39a915fbfdfc7ec9f8fe9da95cd4f3613ac8e
ericoporto <ericoporto2008@gmail.com>
2020-02-15 16:28:16 -0300

    adding a cmake , but it doesn't include generated stuff yet

---
+++ include/tree-sitter/langs.h
@@ -0,0 +1,16 @@
+#ifndef TREE_SITTER_LANGS_H_
+#define TREE_SITTER_LANGS_H_
+
+#include "tree_sitter/runtime.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+const TSLanguage *tree_sitter_ags_script();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
